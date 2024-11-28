9e736ef4525ba82f720c5e8114055b438b1aee72
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-11-28 11:10:18 +0100

    Login Básico

---
+++ Login/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Login.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Validar usuario
+                if (model.Username == "admin" && model.Password == "password")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Usuario o contraseña incorrectos");
+            }
+            return View(model);
+        }
+    }
+}
--- Login/Models/ViewModels/LoginViewModel.cs
+++ Login/Models/ViewModels/LoginViewModel.cs
@@ -1,8 +1,16 @@
-﻿namespace Login.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
+        [Display(Name = "Nombre de usuario")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
     }
 }
